1aebc282dbc46e8eea7273fb67d9f2d59d69e575
jnthn-sltn <jnthn.sltn.wrk@gmail.com>
2018-01-28 14:58:36 -0800

    corrected while loop

--- sensors/readSensors.py
+++ sensors/readSensors.py
@@ -8,18 +8,19 @@ duration = 5
 sensorDHT = 22
 pinDHT = 26
 
-# Try to grab a sensor reading.  Use the read_retry method which will retry up
-# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-humidity, temperature = Adafruit_DHT.read_retry(sensorDHT, pinDHT)
 
-# Un-comment the line below to convert the temperature to Fahrenheit.
-temperature = temperature * 9/5.0 + 32
 
 # Note that sometimes you won't get a reading and
 # the results will be null (because Linux can't
 # guarantee the timing of calls to read the sensor).
 # If this happens try again!
 while 1:
+    # Try to grab a sensor reading.  Use the read_retry method which will retry up
+    # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+    humidity, temperature = Adafruit_DHT.read_retry(sensorDHT, pinDHT)
+
+    # Un-comment the line below to convert the temperature to Fahrenheit.
+    temperature = temperature * 9/5.0 + 32
     if humidity is not None and temperature is not None:
         print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
         time.sleep(duration)

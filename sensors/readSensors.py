75368d0343ea1f3b1fccdfbd6249af5ca2eee690
jnthn-sltn <jnthn.sltn.wrk@gmail.com>
2018-01-28 15:10:49 -0800

    corrected while loop2

--- sensors/readSensors.py
+++ sensors/readSensors.py
@@ -2,13 +2,19 @@
 import sys
 import time
 import Adafruit_DHT
-
+import pymongo
+from datetime import datetime
+from pytz import timezone
 
 duration = 5
 sensorDHT = 22
 pinDHT = 26
 
-
+connection = pymongo.MongoClient("mongodb://admin:admin@ds217898.mlab.com:17898/ambilampdb")
+db = connection.ambilampdb
+sounds = db.sound
+humidities = db.hum
+temperatures = db.temp
 
 # Note that sometimes you won't get a reading and
 # the results will be null (because Linux can't
@@ -21,7 +27,17 @@ while 1:
 
     # Un-comment the line below to convert the temperature to Fahrenheit.
     temperature = temperature * 9/5.0 + 32
+    
+    dto = datetime.now(timezone('UTC'))
+    dto_pacific = dto.astimezone(timezone('US/Pacific'))
+    dts = datetime.strftime(dto_pacific,"%Y-%m-%d %H:%M:%S")
+
+
     if humidity is not None and temperature is not None:
+        humidity_entry = {'time':dts, 'val':humidity}
+        humidities.insert_one(humidity_entry)
+        temperature_entry = {'time':dts, 'val':temperature}
+        temperatures.insert_one(temperature_entry)
         print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
         time.sleep(duration)
 #end

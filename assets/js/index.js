46e31b730e9c752fd230215623f2c0a2db0afa9a
jnthn-sltn <jnthn.sltn.wrk@gmail.com>
2018-02-04 21:56:07 -0800

    updated radar chart 1.3

--- assets/js/index.js
+++ assets/js/index.js
@@ -41,24 +41,30 @@ var soundCtx = document.getElementById("sound-chart").getContext('2d');
 var soundChart = new Chart(soundCtx, {
     type: 'radar',
     data: {
-        labels: ["1200", "0100","0200","0300","0400","0500","0600","0700",
-        "0800","0900","1000","1100"],
-        datasets: [{
-            label: 'Average Sound Level by Hour - Morning',
-            data: soundDataDay,
-            backgroundColor: [
-                'rgba(255, 230, 64, 0.5)'
-            ],
-            borderColor: [
-                'rgba(255,230,64,1)'
-            ],
-        }]
+        labels: ["1200", "0100","0200","0300","0400","0500","0600","0700","0800","0900","1000","1100"],
+        datasets: 
+            [{
+                label: 'Average Sound Level by Hour - Morning',
+                data: soundDataDay,
+                borderColor: 
+                'rgba(255, 230, 64, 1)',
+                backgroundColor: 
+                'rgba(255, 230, 64, 0.5)',
+            },
+            {
+                label: 'Average Sound Level by Hour - Afternoon',
+                data: soundDataNight,
+                borderColor: 
+                'rgba(12, 12, 56, 1)',
+                backgroundColor: 
+                'rgba(12, 12, 56, 0.5)',
+            }]
     },
     options: {
         scale: {
             ticks: {
                 min: soundMin,
-                max: soundMax,
+                max: soundMax
                 }
             },
         responsive: false

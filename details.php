91e21da70ccae940add3274b1687c046a62ff16b
jnthn-sltn <jnthn.sltn.wrk@gmail.com>
2018-02-04 17:04:20 -0800

    added db access through header.php and update to tempchart1.34

--- details.php
+++ details.php
@@ -1,34 +1,6 @@
 <?php
     include "header.php";
 
-    /* Connect to the DB*/
-    $db = connectMongo();
-    $sounds = $db->sound;
-    $temperatures = $db->temp;
-    $soundCursor = $temperatures->find()->sort(array('entry'=> -1))->limit(24);
-    $temperatureCursor = $temperatures->find()->sort(array('entry'=> -1))->limit(24);
-    /*Parse temperature data. We need to form a string representation
-    /*of two arrays, which will become x-y pairs for a line chart.
-    /* This is because Charts.js will need an array, which we will provide
-    /* by assigning this string to a JS variable.*/
-     $temperatureX = "[";
-     $temperatureData = "[";
-     foreach ($temperatureCursor as $doc) {
-         $time = split('[ ]', $doc['time']);//split the date into day and time
-         $temperatureX = $temperatureX . "'" . $time[1] . "',";//put time into the x axis
-         $temperatureData = $temperatureData . $doc['val'] . ",";//add y value - temperature for that time
-     }
-     //strip the trailing commas and add the closing bracket
-     $temperatureX = trim($temperatureX,",");
-     $temperatureX = $temperatureX . "]";
-
-     $temperatureData = trim($temperatureData,",");
-     $temperatureData = $temperatureData . "]";
-     /* End temperature parse*/
-     echo "<script>";
-     echo "var temperatureData = " . $temperatureData . ";";
-     echo "var temperatureX = " . $temperatureX . ";";
-     echo "</script>";
 ?>
 <html>
 <head>
@@ -37,7 +9,36 @@
 </head>
 <body>
     <?php
-	include "header.php";
+        include "header.php";
+        /* Connect to the DB*/
+        $db = connectMongo();
+        $sounds = $db->sound;
+        $temperatures = $db->temp;
+        $soundCursor = $temperatures->find()->sort(array('entry'=> -1))->limit(24);
+        $temperatureCursor = $temperatures->find()->sort(array('entry'=> -1))->limit(24);
+        /*Parse temperature data. We need to form a string representation
+        /*of two arrays, which will become x-y pairs for a line chart.
+        /* This is because Charts.js will need an array, which we will provide
+        /* by assigning this string to a JS variable.*/
+        $temperatureX = "[";
+        $temperatureData = "[";
+        foreach ($temperatureCursor as $doc) {
+            $time = split('[ ]', $doc['time']);//split the date into day and time
+            $temperatureX = $temperatureX . "'" . $time[1] . "',";//put time into the x axis
+            $temperatureData = $temperatureData . $doc['val'] . ",";//add y value - temperature for that time
+        }
+        //strip the trailing commas and add the closing bracket
+        $temperatureX = trim($temperatureX,",");
+        $temperatureX = $temperatureX . "]";
+
+        $temperatureData = trim($temperatureData,",");
+        $temperatureData = $temperatureData . "]";
+        /* End temperature parse*/
+        echo "<script>";
+        echo "var temperatureData = " . $temperatureData . ";";
+        echo "var temperatureX = " . $temperatureX . ";";
+        echo "</script>";
+    
     ?>
     <!-- BUTTONS AND CANVASES -->
     <input type="button" id="temp-btn" class="btn" value="View Temperature Chart" onclick="drawTemp()">

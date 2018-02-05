c7f287dd0dfc0f1390f3b8fdea8d7526e8b79499
jnthn-sltn <jnthn.sltn.wrk@gmail.com>
2018-02-04 23:32:15 -0800

    updated radar chart 1.7

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,38 +1,33 @@
 var tempCtx = document.getElementById("temp-chart").getContext('2d');
 var tempChart = new Chart(tempCtx, {
-    type: 'bar',
+    type: 'radar',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+        labels: ["1200", "0100","0200","0300","0400","0500","0600","0700","0800","0900","1000","1100"],
+        datasets: 
+            [{
+                label: 'Average temperature Level by Hour - Morning',
+                data: temperatureDataDay,
+                borderColor: 
+                'rgba(255, 230, 64, 1)',
+                backgroundColor: 
+                'rgba(255, 230, 64, 0.5)',
+            },
+            {
+                label: 'Average temperature Level by Hour - Afternoon',
+                data: temperatureDataNight,
+                borderColor: 
+                'rgba(12, 12, 56, 1)',
+                backgroundColor: 
+                'rgba(12, 12, 56, 0.5)',
+            }]
     },
     options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
+        scale: {
+            ticks: {
+                min: temperatureMin,
+                max: temperatureMax
                 }
-            }]
-        },
+            },
         responsive: false
     }
 });
